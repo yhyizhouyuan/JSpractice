0fc52a773605f5e88b6e45c552c6601fe5d5f56e
yizhouyuan <2017767351@qq.com>
2017-12-11 18:59:46 +0800

    add

---
+++ 轮播/PPT式轮播/slider.js
@@ -0,0 +1,37 @@
+var fakeData = [
+    {
+        imgSrc: "image/1.jpg",
+        h2Text: "早上好",
+        h3Text: "Morning"
+    },
+    {
+        imgSrc: "image/2.jpg",
+        h2Text: "中午好",
+        h3Text: "Noon"
+    },
+    {
+        imgSrc: "image/3.jpg",
+        h2Text: "早上好",
+        h3Text: "Morning"
+    },
+    {
+        imgSrc: "image/4.jpg",
+        h2Text: "早上好",
+        h3Text: "morning"
+    },
+    {
+        imgSrc: "image/5.jpg",
+        h2Text: "早上好",
+        h3Text: "morning"
+    },
+    {
+        imgSrc: "image/6.jpg",
+        h2Text: "早上好",
+        h3Text: "morning"
+    },
+    {
+        imgSrc: "image/7.jpg",
+        h2Text: "早上好",
+        h3Text: "morning"
+    }
+];
