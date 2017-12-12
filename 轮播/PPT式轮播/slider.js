fa2138b6067f7d7c1f6ea8a2dfe61ebbc93b0eba
yizhouyuan <2017767351@qq.com>
2017-12-12 23:02:54 +0800

    add

--- 轮播/PPT式轮播/slider.js
+++ 轮播/PPT式轮播/slider.js
@@ -35,3 +35,62 @@ var fakeData = [
         h3Text: "morning"
     }
 ];
+// ID/Class 选择器
+var _$ = function (selector) {
+    if (selector.substr(0, 1) === '.') {
+        return document.getElementsByClassName(selector.slice(1));
+    } else if (selector.substr(0, 1) === '#') {
+        return document.getElementById(selector.slice(1));
+    }
+};
+
+
+window.onload = function () {
+    initSliders();
+    switchSlider(0);
+
+    // 撑满DIV容器
+    var pictureDelegate = _$(".picture")[0];
+    pictureDelegate.onload = function () {
+        var h = pictureDelegate.clientHeight + "px";
+        _$("#template_main").style.height = h;
+
+        var main_i_list = _$(".main-i");
+        for (var i = 0; i < main_i_list.length; i++) {
+            main_i_list[i].style.height = h;
+        }
+    };
+};
+
+
+function initSliders() {
+    var tpl_main = '\
+    <div class="main-i {{from_left_or_right}}" id="main_{{index}}">\
+        <img src="{{imgSrc}}" class="picture">\
+        <div class="caption">\
+            <h2>{{h2Text}}</h2>\
+            <h3>{{h3Text}}</h3>\
+        </div>\
+    </div>\
+    '.trim();
+
+    var tpl_ctrl = '\
+    <a class="ctrl-i" href="javascript:switchSlider({{index}});" id="ctrl_{{index}}"><img src="{{imgSrc}}"></a>\
+    '.trim();
+
+    var i, _tpl_main, _tpl_ctrl;
+    var out_main = [], out_ctrl = [];
+    for (i in fakeData) {
+        _tpl_main = tpl_main
+            .replace(/{{index}}/g, i)
+            .replace(/{{imgSrc}}/g, fakeData[i]['imgSrc'])
+            .replace(/{{h2Text}}/g, fakeData[i]['h2Text'])
+            .replace(/{{h3Text}}/g, fakeData[i]['h3Text'])
+            .replace(/{{from_left_or_right}}/g, ['from_right', 'from_left'][i % 2]);
+        out_main.push(_tpl_main);
+
+        _tpl_ctrl = tpl_ctrl
+            .replace(/{{index}}/g, i)
+            .replace(/{{imgSrc}}/g, fakeData[i]['imgSrc']);
+        out_ctrl.push(_tpl_ctrl);
+    }
