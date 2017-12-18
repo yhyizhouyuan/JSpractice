ed04d77c993c4103cffdbf55ea35c66e67037859
yizhouyuan <2017767351@qq.com>
2017-12-18 16:33:13 +0800

    add

---
+++ 变色/clickMe.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+function  clickMe(){
+	var cs1=document.getElementById("c1");
+	cs1.href="2.css";
+	}
+	
+	/*
+·½·¨2£º	window.onload = function () {
+        var oDiv  = document.getElementById('outer').getElementsByTagName('div');
+        var oBtn = document.getElementsByTagName('input')[0];
+        oBtn.onclick = function () {
+            for(var i=0;i<oDiv.length;i++)
+            {
+                oDiv[i].style.background = 'black';
+            }
+        }
+    }
+	*/
